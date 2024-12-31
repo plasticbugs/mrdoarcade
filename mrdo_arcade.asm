3990d53e19d0dcf0bbee76961e1d84ccff382f96
plasticbugs <scottmosch@gmail.com>
2024-12-30 22:16:17 -0800

    fix: remove unused code

--- mrdo_arcade.asm
+++ mrdo_arcade.asm
@@ -11033,32 +11033,6 @@ cvb_INTERMISSION:
 	RET
 
 
-; Input: HL = number to convert, DE = location of ASCII string
-; Output: ASCII string at (DE)
-Num2Dec:
-	ld	bc,-10000
-	call	Num1
-	ld	bc,-1000
-	call	Num1
-	ld	bc,-100
-	call	Num1
-	ld	c,-10
-	call	Num1
-	ld	c,b
-
-Num1:
-	ld	a,'0'-1
-Num2:
-	inc	a
-	add	hl,bc
-	jr	c,Num2
-	sbc	hl,bc
-
-	ld	(de),a
-	inc	de
-	ret
-
-
 ;----------------------------------------------------------------------
 ; PRINT_LEVEL_STATS: Shows scores for current and previous two levels
 ;----------------------------------------------------------------------
